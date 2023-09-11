d440daf0cf2d3570df83f375bc6d70365ce23cf1
Ezequiel Gonzalez <ezequiel.gonzalez.cics@ust.edu.ph>
2023-09-11 11:45:13 +0800

    Lab Activity 1

---
+++ GonzalezITELEC1C/Controllers/InstructorController.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using GonzalezITELEC1C.Models;
+
+namespace GonzalezITELEC1C.Controllers
+{
+    public class InstructorController : Controller
+    {
+        List<Instructor> InstructorList = new List<Instructor>()
+            {
+                new Instructor()
+                {
+                    Id = 1,
+                    FirstName = "Ezequiel",
+                    LastName = "Gonzalez",
+                    IsTenured = true,
+                    Rank = Rank.Professor,
+                    HiringDate = DateTime.Parse("11/09/2019")
+
+
+                 },
+                new Instructor()
+                {
+                     Id = 2,
+                    FirstName = "Kirsten",
+                    LastName = "Zulaybar",
+                    IsTenured = false,
+                    Rank = Rank.Instructor,
+                    HiringDate = DateTime.Parse("11/09/2023")
+
+
+                },
+                new Instructor()
+              {
+                     Id = 3,
+                    FirstName = "Edmund Lecter",
+                    LastName = "Garraton",
+                    IsTenured = true,
+                    Rank = Rank.AssociateProfessor,
+                    HiringDate = DateTime.Parse("11/09/2023")
+              }
+              };
+        public IActionResult Index()
+        {
+           
+            return View(InstructorList);
+        }
+        public IActionResult ShowDetails(int id)
+        {
+            
+            Instructor? instructor = InstructorList.FirstOrDefault(st => st.Id == id);
+
+            if (instructor != null) {
+                return View(instructor);
+            }
+            return NotFound();
+        }
+    }
+}
+            
+        
+    
--- GonzalezITELEC1C/Controllers/StudentController.cs
+++ GonzalezITELEC1C/Controllers/StudentController.cs
@@ -1,12 +1,52 @@
 ﻿using Microsoft.AspNetCore.Mvc;
-
+using GonzalezITELEC1C.Models;
 namespace GonzalezITELEC1C.Controllers
 {
     public class StudentController : Controller
     {
+        List<Student> StudentList = new List<Student>()
+            {
+            new Student()
+            {
+                Id = 1,
+                FirstName = "Ezequiel",
+                LastName = "Gonzalez",
+                Birthday = DateTime.Parse("09/07/2001"),
+                Major = Major.BSIT,
+                Email = "ezequiel.gonzalez.cics@ust.edu.ph"
+            },
+             new Student()
+             {
+                 Id = 2,
+                 FirstName = "Rico",
+                 LastName = "Nieto",
+                 Birthday = DateTime.Parse("04/01/2002"),
+                 Major = Major.BSIT,
+                 Email = "rico.nieto.cics@ust.edu.ph"
+             },
+              new Student()
+              {
+                  Id = 3,
+                  FirstName = "Jakob",
+                  LastName = "Palomo",
+                  Birthday = DateTime.Parse("08/12/2002"),
+                  Major = Major.BSIT,
+                  Email = "jakob.palomo.cics@ust.edu.ph"
+              }
+              };
         public IActionResult Student()
         {
-            return View();
+            return View(StudentList);
+        }
+        public IActionResult ShowDetails(int id)
+        {
+            //Search for the student whose id matches the given id
+            Student? student = StudentList.FirstOrDefault(st => st.Id == id);
+
+            if (student != null)//was an student found?
+                return View(student);
+
+            return NotFound();
         }
     }
 }
